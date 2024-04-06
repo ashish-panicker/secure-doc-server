66c334d3f58a35d04d9951b90964be670c608396
ashish-panicker <ashish.s.panicker@gmail.com>
2024-04-06 18:25:45 +0530

    Created the Auditable entity.

---
+++ src/main/java/com/easyskillup/sdoc/entities/Auditable.java
@@ -0,0 +1,65 @@
+package com.easyskillup.sdoc.entities;
+
+import com.easyskillup.sdoc.exceptions.ApiException;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.AlternativeJdkIdGenerator;
+
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.*;
+
+/**
+ * @author Ashish
+ */
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public abstract class Auditable {
+
+    @Id
+    @SequenceGenerator(name = "primary_id_seq", sequenceName = "primary_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "primary_id_seq")
+    private long id;
+
+    private String refId = new AlternativeJdkIdGenerator().generateId().toString();
+
+    private long updatedBy;
+
+    private long createdBy;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void auditablePrePersist(){
+        Long userId = 1l;
+        if(userId == null){
+            throw new ApiException("Cannot persist the entity with a null user id in the RequestContext.");
+        }
+        setCreatedAt(now());
+        setUpdatedAt(now());
+        setCreatedBy(userId);
+        setUpdatedBy(userId);
+    }
+
+    @PreUpdate
+    public void auditablePreUpdate(){
+        Long userId = 1l;
+        if(userId == null){
+            throw new ApiException("Cannot update the entity with a null user id in the RequestContext.");
+        }
+        setUpdatedAt(now());
+        setUpdatedBy(userId);
+    }
+}
---
+++ src/main/java/com/easyskillup/sdoc/exceptions/ApiException.java
@@ -0,0 +1,7 @@
+package com.easyskillup.sdoc.exceptions;
+
+public class ApiException extends RuntimeException {
+    public ApiException(String message) {
+        super(message);
+    }
+}
