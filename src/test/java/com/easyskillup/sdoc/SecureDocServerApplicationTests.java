3af063ab3f43fee129d9a3409dc53c57f234af83
ashish-panicker <ashish.s.panicker@gmail.com>
2024-04-06 18:03:25 +0530

    Created the server application.

---
+++ src/test/java/com/easyskillup/sdoc/SecureDocServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyskillup.sdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDocServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
